6342037d40c51efa6c1f700daaa4aa3e9376175b
Bogdan <Bogdanufa2006@gmail.com>
2025-05-31 09:44:12 +0300

    Clear

--- New/CommandResult.php
+++ New/CommandResult.php
@@ -1,14 +1,1 @@
 <?php
-Class CommandResult
-{
-    public function getCommand($request)
-    {
-        $reg = Register::instance();
-        $path = $request->getPath();
-        $conf_obj = $reg->getConf();
-        $conf = $conf_obj->get($path);
-        $refclass = new \ReflectionClass($conf);
-        $obj = $refclass->newInstance();
-        return $obj;
-    }
-}
